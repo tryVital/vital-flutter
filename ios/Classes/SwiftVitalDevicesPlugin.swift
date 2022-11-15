6b2d57c976d00b8742f83191a7d60975d36871d1
József Barát <baratjozsef@gmail.com>
2022-11-15 16:29:45 +0100

    cleapup

--- ios/Classes/SwiftVitalDevicesPlugin.swift
+++ ios/Classes/SwiftVitalDevicesPlugin.swift
@@ -319,13 +319,3 @@ public struct InternalScannedDevice: Equatable, Encodable {
     self.deviceModel = deviceModel
   }
 }
-
-extension AnyPublisher {
-  static var empty: AnyPublisher {
-    Empty().eraseToAnyPublisher()
-  }
-
-  func flatMapLatest<P: Publisher>(_ f: @escaping (Self.Output) -> P) -> AnyPublisher<P.Output, P.Failure> where P.Failure == Self.Failure {
-    map(f).switchToLatest().eraseToAnyPublisher()
-  }
-}
