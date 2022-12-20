7416d2b5ac726da72f02e437444a3af37345512a
József Barát <baratjozsef@gmail.com>
2022-12-20 13:18:45 +0100

    feat: VIT-2193 expose all resources (#23)

--- packages/vital_core/lib/services/data/vitals.dart
+++ packages/vital_core/lib/services/data/vitals.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+
 part 'vitals.g.dart';
 
 @JsonSerializable(createToJson: false)
@@ -17,7 +18,30 @@ class Measurement {
     this.unit,
   });
 
-  factory Measurement.fromJson(Map<String, dynamic> json) => _$MeasurementFromJson(json);
+  factory Measurement.fromJson(Map<String, dynamic> json) =>
+      _$MeasurementFromJson(json);
+}
+
+@JsonSerializable(createToJson: false)
+class BloodPressureMeasurement {
+  int? id;
+  DateTime timestamp;
+  double systolic;
+  double diastolic;
+  String? type;
+  String? unit;
+
+  BloodPressureMeasurement({
+    required this.id,
+    required this.timestamp,
+    required this.systolic,
+    required this.diastolic,
+    this.type,
+    this.unit,
+  });
+
+  factory BloodPressureMeasurement.fromJson(Map<String, dynamic> json) =>
+      _$BloodPressureMeasurementFromJson(json);
 }
 
 enum CholesterolType {
