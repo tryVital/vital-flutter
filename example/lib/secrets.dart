21e773438c2846f1ba1b028b426f16c621130a15
Anders Ha <anders@tryvital.io>
2023-10-05 16:27:21 +0100

    VIT-4643: Update Flutter example app to drop deprecated api usage (#62)

---
+++ example/lib/secrets.dart
@@ -0,0 +1,7 @@
+import 'package:vital_core/vital_core.dart';
+
+// Replace this with your own API key
+const apiKey = 'sk_us_......';
+
+const region = Region.us;
+const environment = Environment.sandbox;
