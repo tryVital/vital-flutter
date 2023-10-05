6dcc8869fa2c3495448c4f736cb7c19d5f5cc8a5
Anders Ha <anders@tryvital.io>
2023-10-05 15:10:04 +0100

    VIT-4638: vital_core iOS platform interface (#59)

--- packages/vital_core/vital_core_ios/ios/Classes/SwiftVitalCorePlugin.swift
+++ packages/vital_core/vital_core_ios/ios/Classes/SwiftVitalCorePlugin.swift
@@ -3,36 +3,175 @@ import UIKit
 import VitalCore
 
 public class SwiftVitalCorePlugin: NSObject, FlutterPlugin {
-    private let channel: FlutterMethodChannel
+  private let channel: FlutterMethodChannel
 
-    private var flutterRunning = true
+  private var flutterRunning = true
 
-    init(_ channel: FlutterMethodChannel){
-        self.channel = channel
+  init(_ channel: FlutterMethodChannel){
+    self.channel = channel
+  }
+
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "vital_core", binaryMessenger: registrar.messenger())
+    let instance = SwiftVitalCorePlugin(channel)
+
+    registrar.publish(instance)
+
+    registrar.addMethodCallDelegate(instance, channel: channel)
+    registrar.addApplicationDelegate(instance)
+  }
+
+  public func detachFromEngine(for registrar: FlutterPluginRegistrar) {
+    flutterRunning = false
+  }
+
+  public func applicationWillTerminate(_ application: UIApplication) {
+    flutterRunning = false
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    Task {
+      func reportInvalidArguments(context: String = "") {
+        result(
+          FlutterError(
+            code: "InvalidArgument",
+            message: "Invalid arguments for \(call.method)" + (context != "" ? ": \(context)" : ""),
+            details: nil
+          )
+        )
+      }
+
+      func reportError(_ error: Error) {
+        result(
+          FlutterError(
+            code: "VitalCoreError",
+            message: error.localizedDescription,
+            details: nil
+          )
+        )
+      }
+
+      switch call.method {
+      case "setUserId":
+        guard let argument = call.arguments as? String, let userId = UUID(uuidString: argument) else {
+          return reportInvalidArguments()
+        }
+        await VitalClient.setUserId(userId)
+        result(nil)
+
+      case "configure":
+        guard
+          let arguments = call.arguments as? [String: AnyHashable],
+          let rawEnvironment = arguments["environment"] as? String,
+          let region = (arguments["region"] as? String).flatMap(Environment.Region.init(rawValue:)),
+          let apiKey = arguments["apiKey"] as? String
+        else { return reportInvalidArguments() }
+
+        let environment: Environment
+        switch rawEnvironment {
+        case "dev":
+          environment = .dev(region)
+        case "sandbox":
+          environment = .sandbox(region)
+        case "production":
+          environment = .production(region)
+        default:
+          reportInvalidArguments(context: "unknown environment: \(rawEnvironment)")
+          return
+        }
+
+        await VitalClient.configure(apiKey: apiKey, environment: environment)
+        result(nil)
+
+      case "signIn":
+        guard
+          let arguments = call.arguments as? [String: AnyHashable],
+          let signInToken = arguments["signInToken"] as? String
+        else { return reportInvalidArguments() }
+
+        do {
+          try await VitalClient.signIn(withRawToken: signInToken)
+          result(nil)
+
+        } catch let error {
+          reportError(error)
+        }
+
+      case "hasUserConnectedTo":
+        guard
+          let arguments = call.arguments as? [String: AnyHashable],
+          let rawProvider = arguments["provider"] as? String
+        else { return reportInvalidArguments() }
+
+        guard let provider = Provider.Slug(rawValue: rawProvider)
+          else { return reportInvalidArguments(context: "unrecognized provider \(rawProvider)") }
+
+        do {
+          result(try await VitalClient.shared.isUserConnected(to: provider))
+
+        } catch let error {
+          reportError(error)
+        }
+
+      case "userConnectedSources":
+        do {
+          let providers = try await VitalClient.shared.user.userConnectedSources()
+            .map { ["name": $0.name, "slug": $0.slug.rawValue, "logo": $0.logo] }
+          let jsonString = String(data: try JSONEncoder().encode(providers), encoding: .utf8)!
+          // NOTE: Dart end expects a JSON string
+          result(jsonString)
+
+        } catch let error {
+          reportError(error)
+        }
+
+      case "createConnectedSourceIfNotExist":
+        guard
+          let arguments = call.arguments as? [String: AnyHashable],
+          let rawProvider = arguments["provider"] as? String
+        else { return reportInvalidArguments() }
+
+        guard let provider = Provider.Slug(rawValue: rawProvider)
+          else { return reportInvalidArguments(context: "unrecognized provider \(rawProvider)") }
+
+        do {
+          try await VitalClient.shared.checkConnectedSource(for: provider)
+          result(nil)
+
+        } catch let error {
+          reportError(error)
+        }
+
+      case "deregisterProvider":
+        guard
+          let arguments = call.arguments as? [String: AnyHashable],
+          let rawProvider = arguments["provider"] as? String
+        else { return reportInvalidArguments() }
+
+        guard let provider = Provider.Slug(rawValue: rawProvider)
+          else { return reportInvalidArguments(context: "unrecognized provider \(rawProvider)") }
+
+        do {
+          try await VitalClient.shared.user.deregisterProvider(provider: provider)
+          result(nil)
+
+        } catch let error {
+          reportError(error)
+        }
+
+      case "cleanUp":
+        await VitalClient.shared.cleanUp()
+        result(nil)
+
+      default:
+        result(
+          FlutterError(
+            code: "UnsupportedMethod",
+            message: "Method not supported \(call.method)",
+            details: nil
+          )
+        )
+      }
     }
-
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "vital_core", binaryMessenger: registrar.messenger())
-        let instance = SwiftVitalCorePlugin(channel)
-
-        registrar.publish(instance)
-
-        registrar.addMethodCallDelegate(instance, channel: channel)
-        registrar.addApplicationDelegate(instance)
-    }
-
-    public func detachFromEngine(for registrar: FlutterPluginRegistrar) {
-        flutterRunning = false
-    }
-
-    public func applicationWillTerminate(_ application: UIApplication) {
-        flutterRunning = false
-    }
-
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        result(FlutterError.init(code: "Unsupported method",
-                                 message: "Method not supported \(call.method)",
-                                 details: nil))
-    }
-
+  }
 }
--- packages/vital_core/vital_core_platform_interface/lib/src/vital_core_method_channel.dart
+++ packages/vital_core/vital_core_platform_interface/lib/src/vital_core_method_channel.dart
@@ -1,7 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:fimber/fimber.dart';
 import 'package:flutter/services.dart';
 import 'package:vital_core_platform_interface/vital_core_platform_interface.dart';
 
@@ -17,5 +13,51 @@ class VitalCoreMethodChannel extends VitalCorePlatform {
       }
       return null;
     });
+  }
+
+  @override
+  Future<void> setUserId(String userId) {
+    return _channel.invokeMethod("setUserId", userId);
+  }
+
+  @override
+  Future<void> configure(String apiKey, String environment, String region) {
+    return _channel.invokeMethod("configure",
+        {"apiKey": apiKey, "environment": environment, "region": region});
+  }
+
+  @override
+  Future<void> signIn(String signInToken) {
+    return _channel.invokeMethod("signIn", {"signInToken": signInToken});
+  }
+
+  @override
+  Future<bool> hasUserConnectedTo(String provider) {
+    return _channel.invokeMethod("hasUserConnectedTo",
+        {"provider": provider}).then((value) => value as bool);
+  }
+
+  @override
+  Future<String> userConnectedSources() {
+    // NOTE: Native to respond with a JSON string
+    return _channel
+        .invokeMethod("userConnectedSources")
+        .then((value) => value as String);
+  }
+
+  @override
+  Future<void> createConnectedSourceIfNotExist(String provider) {
+    return _channel.invokeMethod(
+        "createConnectedSourceIfNotExist", {"provider": provider});
+  }
+
+  @override
+  Future<void> deregisterProvider(String provider) {
+    return _channel.invokeMethod("deregisterProvider", {"provider": provider});
+  }
+
+  @override
+  Future<void> cleanUp() {
+    return _channel.invokeMethod("cleanUp");
   }
 }
