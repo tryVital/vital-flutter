2a383ba11746268002623eb394257ec0d8ca1235
Anders Ha <anders@tryvital.io>
2024-01-11 15:04:59 +0000

    VIT-5547: Migrate VitalHealth.ask to use ActivityResultRegistry for Android 14 Permission Manager compatibility

--- example/android/app/src/main/kotlin/com/example/vital_flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/vital_flutter_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.vital_flutter_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
