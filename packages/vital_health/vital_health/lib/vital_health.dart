434df92982607c038634716fefab36087ddc88c8
Anders Ha <anders@tryvital.io>
2024-02-14 11:35:02 +0000

    CS1-142: vital_health should re-export ProcessedData types

--- packages/vital_health/vital_health/lib/vital_health.dart
+++ packages/vital_health/vital_health/lib/vital_health.dart
@@ -4,3 +4,4 @@ export 'package:vital_health_platform_interface/src/data/health_config.dart';
 export 'package:vital_health_platform_interface/src/data/permission_outcome.dart';
 export 'package:vital_health_platform_interface/src/data/sync_data.dart';
 export 'package:vital_health_platform_interface/src/health_resource.dart';
+export 'package:vital_health_platform_interface/src/data/processed_data/processed_data.dart';
