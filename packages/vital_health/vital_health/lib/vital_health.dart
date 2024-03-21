f3e7ace547d5bfa5de058887e1a55069a126c683
Anders Ha <anders@tryvital.io>
2024-03-21 19:33:27 +0000

    VIT-6007: Flutter Android Background Sync API Tweaks

--- packages/vital_health/vital_health/lib/vital_health.dart
+++ packages/vital_health/vital_health/lib/vital_health.dart
@@ -5,3 +5,4 @@ export 'package:vital_health_platform_interface/src/data/permission_outcome.dart
 export 'package:vital_health_platform_interface/src/data/sync_data.dart';
 export 'package:vital_health_platform_interface/src/health_resource.dart';
 export 'package:vital_health_platform_interface/src/data/processed_data/processed_data.dart';
+export 'package:vital_health_platform_interface/src/data/sync_notification_content.dart';
