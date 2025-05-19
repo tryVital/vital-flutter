43c03dae70505ad2aa0ffb7801da35f41f695f5f
Anders Ha <anders@junction.com>
2025-05-19 17:43:21 +0100

    DEV-483: Add integration tests for permissionStatus

---
+++ example/integration_test/resource_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:vital_health/vital_health.dart' as vital_health;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets("test permissionStatus round-trip", (WidgetTester tester) async {
+    var result = await vital_health.permissionStatus(vital_health.HealthResource.values);
+    expect(result.keys.toSet(), equals(vital_health.HealthResource.values.toSet()));
+  });
+}
